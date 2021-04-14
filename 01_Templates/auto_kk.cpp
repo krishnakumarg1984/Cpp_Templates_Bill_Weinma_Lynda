2db4a4dc8b7733e24412d141595b5e629207613e
Krishnakumar Gopalakrishnan <krishna.kumar@ucl.ac.uk>
2021-04-14 18:40:22 +0100

    modifies the auto_kk.cpp file

--- 01_Templates/auto_kk.cpp
+++ 01_Templates/auto_kk.cpp
@@ -7,13 +7,14 @@ using namespace std;
 
 int main() {
     // int i = 47;
-    // const char *cstr = "this is a c-string.";
+    const char* cstr = "this is a c-string.";
     // const string sclass = string("this is a string class string.");
 
     auto x{"this is a string"};
     decltype(x) y;
     std::cout << "the type of cstr is:" << typeid(cstr).name() << std::endl;
     std::cout << "the type of x is:" << typeid(x).name() << std::endl;
+    std::cout << "the type of y is:" << typeid(y).name() << std::endl;
 
     return 0;
 }
